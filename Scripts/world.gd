aa9e733ebc55e9c06efc71fb9183f7c3d31d3a8c
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-06-14 16:01:49 -0700

    Internet connect
    Theoretically we can now use upnp to share address and connect over internet. Cool but need to export before trying

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -26,14 +26,17 @@ func _on_host_button_pressed() -> void:
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
 	
 	add_player(multiplayer.get_unique_id())
+	
+	upnp_setup()
 
 func _on_join_button_pressed() -> void:
 	main_menu.hide()
 	hud.show()
 	
-	enet_peer.create_client("localhost", PORT)
+	enet_peer.create_client(address_in.text, PORT)
 	multiplayer.multiplayer_peer = enet_peer
 
 func add_player(peer_id):
@@ -42,9 +45,32 @@ func add_player(peer_id):
 	player.set_multiplayer_authority(peer_id)
 	add_child(player)
 
+func remove_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+
 func _update_health_bar(health : float):
 	health_bar.value = health
 
 func _on_multiplayer_spawner_spawned(node: Node) -> void:
 	if node.is_multiplayer_authority():
 		node.health_changed.connect(_update_health_bar)
+
+# Connection over internet is below
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	
+	var discover_result = upnp.discover()
+	# Check if upnp is good
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Discover failed! Error %s" % discover_result) 
+	
+	#check if gateway is good
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), "UPNP Invalid Gateway!")
+	
+	var map_result = upnp.add_port_mapping(PORT)
+	# Check port mapping works
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Port Mapping failed! Error %s" % map_result)
+	
+	print("Success! Have others join this address: %s" % upnp.query_external_address()[randi() % 10])
