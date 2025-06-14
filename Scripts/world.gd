d2f48eff8712be9b6fa9a7a48e8f71a245a11fe4
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-06-14 14:42:46 -0700

    Downloaded Wakatime
    FORGOT NOOO

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,6 +1,13 @@
 extends Node
 
+@onready var main_menu :  Control= $"CanvasLayer/Main Menu"
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 		
+func _on_host_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_join_button_pressed() -> void:
+	pass # Replace with function body.
